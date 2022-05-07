8e19637bda23258adde9bdbb927200c2cdb09b09
Ronolg <259369@student.pwr.edu.pl>
2022-05-07 21:41:13 +0200

    bucket complete, quicksort is unable to sort more than aprox. 500 000 elements due to limited stack memory, merge sort works when size of sorting array is equivalent to 2^(number)

--- sorting.cpp
+++ sorting.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <array>
 
 
-const long long int size = 400000;
+const long long int size = 2*262144; //rozmiar sortowanej tablicy
+//const int max_id = 1010291; //id ostatniego elementu w danych
 
 class Movie
 {
     private:
         int id;
-        std::string title;
         double raiting;
     public:
     void setId(int a){this->id = a;}
-    void setTitle(std::string t){this->title = t;}
     void setRaiting(double r){this->raiting = r;}
 
     int getId(){return this->id;}
-    std::string getTitle(){return this->title;}
     double getRaiting(){return this->raiting;}
 
     Movie()
@@ -30,7 +27,6 @@ class Movie
     Movie operator=(Movie b)
     {
         this->setId(b.getId());
-        this->setTitle(b.getTitle());
         this->setRaiting(b.getRaiting());
 
         return *this;
@@ -52,16 +48,14 @@ Movie* read(Movie *array)
     std::string line_skipped;
     char character; 
     int id_read; 
-    std::string title_read;
     double raiting_read;
     bool raiting_found=0;
     bool next=0;
     int i=0;
-    int j=0;
+    int quotemark_count = 0;
 
     std::string raiting_string; // variable used to read raitings
     std::string id_string; // variable used to read id's
-    std::string title_string; // variable used to read titles
 
 
 
@@ -69,7 +63,7 @@ Movie* read(Movie *array)
     {
         getline(file,line_skipped); // skip first line: ",movie,rating"
         
-        while(i<size)
+        while(i<size && !file.eof())
         {   
             character = file.get();
 
@@ -92,64 +86,80 @@ Movie* read(Movie *array)
             // reading title and raiting
             while(!raiting_found && character!='\n' && !next)
             {
-                if(character == ',')
-                {    
-                    character = file.get();
-                    if(character=='\n')
-                        next=1;
+                
+                //character = file.get();
+                
+                if(character == '"') 
+                    quotemark_count++; //when " is found, count it 
+                
 
-                   
+                if(character==',' && quotemark_count!=1 && !next)
+                {
+                    character = file.get();
                     if(character<=57 && character>47)
                     {
-                        raiting_found=1;  
                         file >> raiting_string;
                         raiting_read = std::stod(character+raiting_string);
+                        raiting_found=1;
                     }
-                    character='\0';
-                }
+                    //next=1;
+
+                }    
+                if(character=='\n') //check if it's end of the line
+                    next=1;
+                
                 if(!next)
-                {
-                    title_read+=character;
                     character = file.get();
-                }
             }
             
             if(raiting_found && id_read!=-1)
             {  
-                for(j=0; j<=i ; j++)
-                {
-                    if(array[j].getRaiting()==-1)
-                    { 
-                        array[j].setId(id_read);
-                        array[j].setTitle(title_read);
-                        array[j].setRaiting(raiting_read);
-                    }
+                while(i>0 && array[i-1].getRaiting()==-1)
+                    i--;
+                
+                array[i].setId(id_read);
+                array[i].setRaiting(raiting_read);
+                
+              /*   if(i>0 && array[i-1].getRaiting()==-1)
+                { 
+                    array[i-1].setId(id_read);
+                    array[i-1].setRaiting(raiting_read);
+                    i--;
                 }
+                else
+                {
+                    array[i].setId(id_read);
+                    array[i].setRaiting(raiting_read);
+                } */
             }
             
             raiting_found=0;
             next=0;
             id_read=-1;
             raiting_read=-1;
+            quotemark_count=0;
  
+
+            
+            //std::cout << "Id: " << array[i].getId()<< std::endl;
+            //std::cout << "i: " << i << std::endl; 
+
 
              
             /* std::cout << "id_read: "<< id_read <<std::endl;
-            std::cout << "title_read: "<< title_read <<std::endl;
             std::cout << "raiting_read: "<< raiting_read <<std::endl;
 
             std::cout << std::endl;
 
             std::cout << "id_array: "<< array[i].getId()<<std::endl;
-            std::cout << "title_array: "<< array[i].getTitle() <<std::endl;
             std::cout << "raiting_array: "<< array[i].getRaiting() <<std::endl;
 
-            std::cout << "===============================" <<std::endl; */
-
+            std::cout << "===============================" <<std::endl;
+  */    
             i++;
-            title_read.clear();
             id_string.clear();
         };
+
     }
     file.close();
 
@@ -208,51 +218,6 @@ void quickSort(Movie *array, int first, int last)
     }
 }
 
-void bucketSort(Movie *array)
-{
-    Movie* bucket_array[10];
-    int max_raiting=10;
-    int counters[10]={};
-    int default_size = size/5;
-    int current_size = default_size;
-    
-    for(int i = 0; i<max_raiting; i++)
-        bucket_array[i] = new Movie[default_size];
- 
-    for(int i = 0; i<size; i++)
-    {
-        
-        if(array[i].getRaiting()<=10)
-        {
-
-                if(counters[(int)array[i].getRaiting()-1]>=current_size) //check if bucket has enough memory to store another element
-                {
-                    Movie* copy = bucket_array[(int)array[i].getRaiting()-1]; // create a copy of right bucket
-                    current_size = counters[(int)array[i].getRaiting()-1]+default_size;
-                    bucket_array[(int)array[i].getRaiting()-1] = new Movie[current_size]; // allocate more memory for the bucket
-                    
-                    for(int k=0; k<counters[(int)array[i].getRaiting()-1]; k++)
-                        bucket_array[(int)array[i].getRaiting()-1][k] = copy[k];
-                   
-                    delete[] copy;
-                }
-                bucket_array[(int)array[i].getRaiting()-1][counters[(int)array[i].getRaiting()-1]] = array[i];
-                counters[(int)array[i].getRaiting()-1]++;   
-        }
-    }
-  
-    
-    int idx=0;
-    for(int i = 0; i<max_raiting; i++)
-        for(int j = 0; j<counters[i]; j++)
-            array[idx++]= bucket_array[i][j];
-    
-    //free used memory
-    for(int i = 0; i<max_raiting; i++)
-        delete[] bucket_array[i];
-
-}
-
 void bucketSort_object(Movie *array)
 {
     Bucket bucket_array[10];
@@ -268,7 +233,7 @@ void bucketSort_object(Movie *array)
     for(int i = 0; i<size; i++)
     {
         
-        if(array[i].getRaiting()<=10)
+        if(array[i].getRaiting()<=10 && array[i].getRaiting()>=0)
         {
             if(bucket_array[(int)array[i].getRaiting()-1].size >= bucket_array[(int)array[i].getRaiting()-1].capacity)
             {
@@ -292,17 +257,86 @@ void bucketSort_object(Movie *array)
         for(int j = 0; j<bucket_array[i].size; j++)
             array[idx++]= bucket_array[i].movies[j];
 
-    std::cout << "Sizes: "<< std::endl;
-    
-    for(int i = 0; i<max_raiting; i++)
-        std::cout << bucket_array[i].size << " ";
-    std::cout << std::endl;
     
     //free used memory
     for(int i = 0; i<max_raiting; i++)
         delete[] bucket_array[i].movies;
 
 }
+
+void merge(Movie *merged_array, Movie* array_left, Movie* array_right, int sizeLeft, int sizeRight)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while(i<sizeLeft && j<sizeRight)
+    {
+        if(array_left[i].getRaiting() <= array_right[j].getRaiting())
+        {
+            merged_array[k] = array_left[i];
+            i++;
+        }
+        else
+        {
+            merged_array[k] = array_right[j];
+            j++;
+        }
+        k++;
+    };
+
+    while(i<sizeLeft) //add leftovers of left array to merged array if there are any
+    {
+        merged_array[k] = array_left[i];
+        i++; k++;
+    };
+
+    while(j<sizeRight) //add leftovers of right array to merged array if there are any
+    {
+        merged_array[k] = array_right[j];
+        j++; k++;
+    };
+}
+
+Movie* divideArray(Movie *array, int divided_size , bool which_half /*0 -> 1st half, 1 -> 2nd half*/)
+{
+    Movie* divided_array = new Movie[divided_size];
+
+    if(!which_half)
+    {
+        for(int i=0; i<divided_size; i++)
+            divided_array[i] = array[i];
+    }
+    else
+    {
+        for(int i=0; i<divided_size; i++)
+            divided_array[i] = array[divided_size+i];
+    }
+
+    
+    return divided_array;
+}   
+
+void mergeSort(Movie *array, int array_size)
+{
+    int leftSize = array_size/2;
+    int rightSize = array_size/2;
+
+    Movie* leftHalf = divideArray(array, leftSize, 0);
+    Movie* rightHalf = divideArray(array, rightSize, 1);
+    
+    if(array_size<2)
+        return;
+
+
+    mergeSort(leftHalf, leftSize);
+    mergeSort(rightHalf, rightSize);
+    merge(array, leftHalf, rightHalf, leftSize, rightSize);
+
+} 
+
+
+
 int main()
 {
     Movie *array = new Movie[size];
@@ -314,33 +348,16 @@ int main()
     std::cout << std::endl;
     std::cout << std::endl;
 
-
-
-    /* for(int i=0;i<size; i++)
-        std::cout<< i <<": "<< array[i].getTitle()<<std::endl;
-
-    std::cout << std::endl;
- */
-    /* std::array<Movie,size> arr;
-    for(int i=0; i<size; i++)
-        arr[i]=array[i];
-    
-    std::sort(arr.begin(), arr.end(),[](Movie& a, Movie& b) -> bool {return a.getRaiting() > b.getRaiting();}); */
- 
-    quickSort(array, 0, size-1);
-    //bucketSort(array);
+    //quickSort(array, 0, size-1);
     //bucketSort_object(array);
-    
+    mergeSort(array,size);
+
     for(int i=0;i<size; i++)
-        std::cout<< array[i].getRaiting() <<" "; //<< "id: " << array[i].getId() << " ";
+        std::cout<< array[i].getRaiting() <<" ";//<< "id: " << array[i].getId() << " ";
 
     std::cout << std::endl;
 
-    /* for(int i=0;i<size; i++)
-        std::cout<< i <<": "<< array[i].getTitle()<<std::endl;
-    
-    std::cout << std::endl;
-    */
-    delete[] array;
+    delete[] array; 
+
     return 0;
 }
